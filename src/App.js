8c8416e8359c597fbeb854927f0b585cadc23def
Aleksandar15 <58865226+Aleksandar15@users.noreply.github.com>
2022-10-06 01:34:13 +0200

    Ditched React.lazy() as now my Loading's are soaking up all the necessary time it takes until the correct page is shown or redirects to the correct route

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { Fragment, Suspense, lazy } from "react";
+import { Fragment } from "react";
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
@@ -7,13 +7,12 @@ import React from "react";
 import NavBar from "./components/NavBar/NavBar";
 import PageNotFound from "./components/PageNotFound/PageNotFound";
 import PersistLogin from "./components/PersistLogin/PersistLogin";
+import Login from "./components/Login";
+import HomePage from "./components/HomePage";
+import Register from "./components/Register";
+import Manager from "./components/manager/Manager";
 
 const App = () => {
-  const FallbackManager = lazy(() => import("./components/manager/Manager"));
-  const FallbackLogin = lazy(() => import("./components/Login"));
-  const FallbackRegister = lazy(() => import("./components/Register"));
-  const FallbackHomepage = lazy(() => import("./components/HomePage"));
-
   return (
     <Fragment>
       <Router>
@@ -21,52 +20,15 @@ const App = () => {
         <div className="container">
           {/* Public routes */}
           <Routes>
-            <Route
-              path="/"
-              element={
-                <Suspense
-                  fallback={<h3 style={{ textAlign: "center" }}>Loading...</h3>}
-                >
-                  <FallbackHomepage />
-                </Suspense>
-              }
-            />
+            <Route path="/" element={<HomePage />} />
 
-            <Route
-              path="/login"
-              element={
-                <Suspense
-                  fallback={<h3 style={{ textAlign: "center" }}>Loading...</h3>}
-                >
-                  <FallbackLogin />
-                </Suspense>
-              }
-            />
-            <Route
-              path="/register"
-              element={
-                <Suspense
-                  fallback={<h3 style={{ textAlign: "center" }}>Loading...</h3>}
-                >
-                  <FallbackRegister />
-                </Suspense>
-              }
-            />
+            <Route path="/login" element={<Login />} />
+
+            <Route path="/register" element={<Register />} />
 
             {/* Protected routes */}
             <Route element={<PersistLogin />}>
-              <Route
-                path="/manager"
-                element={
-                  <Suspense
-                    fallback={
-                      <h3 style={{ textAlign: "center" }}>Loading...</h3>
-                    }
-                  >
-                    <FallbackManager />
-                  </Suspense>
-                }
-              />
+              <Route path="/manager" element={<Manager />} />
             </Route>
 
             {/* Not found route */}
