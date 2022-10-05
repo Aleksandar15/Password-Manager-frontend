3c7e5a7122944d9720a12d2c706d54f310f62e52
Aleksandar15 <58865226+Aleksandar15@users.noreply.github.com>
2022-10-06 01:42:50 +0200

    Beautifications on Loading screen

--- src/components/Loading/Loading.js
+++ src/components/Loading/Loading.js
@@ -1,5 +1,19 @@
 function Loading() {
-  return <h2 style={{ textAlign: "center" }}>Loading...</h2>;
+  return (
+    <h2
+      style={{
+        textAlign: "center",
+        background: "cyan",
+        color: "blue",
+        borderRadius: "22px",
+        padding: "5px",
+        paddingBottom: "10px",
+        marginTop: "10px",
+      }}
+    >
+      Loading...
+    </h2>
+  );
 }
 
 export default Loading;
